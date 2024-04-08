1c6251ae860d8b67bfe6e6081c125b99a4625287
imotro <139806892+imotro@users.noreply.github.com>
2024-04-08 23:09:47 +0000

    Update Security Policy

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var Unblocker = require('unblocker');
 var app = express();
+const helmet = require('helmet');
 var unblocker = new Unblocker({prefix: '/boom/'});
 app.use(unblocker);
 app.use(express.static('public'))
-
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "default-src 'self' *");
+  next();
+});
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
